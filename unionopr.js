98d80cbf604614e8fcccbcb1a7402cc516d03bf3
Priyanka <priyanka.sjkv@gmail.com>
2023-08-03 14:52:12 +0530

    third commit- multiple datatypes unionopr.ts

---
+++ unionopr.js
@@ -0,0 +1,5 @@
+var empid;
+empid = 1001;
+console.log(empid);
+empid = "pss";
+console.log(empid);
---
+++ unionopr.ts
@@ -0,0 +1,5 @@
+var empid : number | string | boolean;
+empid=1001;
+console.log(empid);
+empid="pss1001";
+console.log(empid);
