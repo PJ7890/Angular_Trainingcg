cb81a03a9dae5b61e95b797f46d1e80083acd586
Priyanka <priyanka.sjkv@gmail.com>
2023-08-04 16:50:11 +0530

    sixth commit - loadmodule - template literal string

--- loadmodule.js
+++ loadmodule.js
@@ -5,3 +5,8 @@ var classdemo_1 = require("./classdemo");
 var circle = new classdemo_1.Circle(5);
 console.log(circle.getarea());
 console.log(classdemo_1.pi);
+//Template lit string
+//so whenever there is needed of combination of string and integer, we can make use of Template lit string
+var username = "marry";
+var age = 24;
+console.log("Heloo ".concat(username, " your age is ").concat(age));
--- loadmodule.ts
+++ loadmodule.ts
@@ -4,3 +4,10 @@ import { Circle, pi } from "./classdemo";
 var circle=new Circle(5); 
 console.log(circle.getarea());
 console.log(pi);
+
+
+//Template lit string
+//so whenever there is needed of combination of string and integer, we can make use of Template lit string
+var username:string= "marry";
+var age:number=24;
+console.log(`Heloo ${username} your age is ${age}`);
